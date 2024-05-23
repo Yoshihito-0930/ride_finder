0aeea2a8e742115309fc1ed85a0a9aa7dc37f855
Yoshihito <930yoshihito@gmail.com>
2024-05-23 14:35:16 +0900

    tailwind daisyui追加

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem "sprockets-rails"
 gem "pg", "~> 1.1"
 
 gem 'fly'
+
+gem 'tailwindcss-rails'
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
---
+++ config/tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim,js}',
+    './app/assets/stylesheets/**/*.css',
+    './node_modules/daisyui/dist/**/*.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      "light"
+    ],
+  },
+}
