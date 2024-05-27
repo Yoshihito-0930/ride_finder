009d1c15eddc6249376f2ac6e8b41b7b509ce1cc
Yoshihito <930yoshihito@gmail.com>
2024-05-27 23:13:56 +0900

    フラッシュメッセージの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
+
+  def require_login
+    return if session[:user_id]
+
+    redirect_to lets_login_path
+  end
+
+  def current_user
+    return unless session[:user_id]
+
+    @current_user ||= User.find_by(uid: session[:user_id])
+  end
+
   def logged_in?
     !!session[:user_id]
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
+    flash[:success] = 'ログインしました'
     redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:info] = 'ログアウトしました'
     redirect_to root_path, status: :see_other
   end
 end
