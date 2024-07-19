254e0861df784d3783b9222ddac6f376db3241a5
Yoshihito <930yoshihito@gmail.com>
2024-07-19 20:03:28 +0900

    検索カテゴリー(ライダーズカフェ)を追加

--- app/models/visit_category.rb
+++ app/models/visit_category.rb
@@ -1,4 +1,4 @@
 class VisitCategory < ApplicationRecord
   has_many :destinations
-  enum name: { scenic_spot: 0, tourist_attraction: 1, roadside_station: 2, campground: 3, viewpoint: 4}
+  enum name: { scenic_spot: 0, tourist_attraction: 1, roadside_station: 2, campground: 3, viewpoint: 4, riders_cafe: 5}
 end
