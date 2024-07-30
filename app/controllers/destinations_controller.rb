98b8afc7904cc94be1bdb375930b1ccd34db4952
Yoshihito <930yoshihito@gmail.com>
2024-07-31 07:19:36 +0900

    リファクタリング

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,23 +3,7 @@ class DestinationsController < ApplicationController
   def create
     place_data = JSON.parse(params[:place_data])
 
-    #複数のユーザーが同時に同じplaceで実行されるとデータが競合状態になる可能性もあるからfind_or_create_by! メソッドを使用するか、トランザクションを使用することを検討
-    @destination = Destination.find_or_create_by(google_maps_place_id: place_data['place_id']) do |destination|
-  
-      destination.name = place_data['name']
-      destination.address = place_data['formatted_address']
-      destination.latitude = place_data['geometry']['location']['lat']
-      destination.longitude = place_data['geometry']['location']['lng']
-      destination.rating = place_data['rating']
-  
-      destination.image = place_data['photo_url'] if place_data['photo_url'].present?
-  
-      if place_data['opening_hours'] && place_data['opening_hours']['weekday_text']
-        destination.business_hours = place_data['opening_hours']['weekday_text'].join("\n")
-      end
-  
-      destination.user_ratings_total = place_data['user_ratings_total']
-    end
+    @destination = Destination.find_or_create_from_place_data(place_data)
     if @destination.save
       redirect_to destination_path(@destination, place_id: @destination.google_maps_place_id)
     else
@@ -30,6 +14,10 @@ class DestinationsController < ApplicationController
 
   def show
     @destination = Destination.find_by(google_maps_place_id: params[:place_id])
+  end
+
+  def favorite_destinations
+    @favorite_destinations = Destination.find_by(id: params[:id])
   end
 
 
