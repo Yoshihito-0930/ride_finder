99309646e728724948c129d76c29d8b98c3d0ae1
Yoshihito <930yoshihito@gmail.com>
2024-08-04 20:05:13 +0900

    showページへの遷移のパラメータの変更

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,25 +1,25 @@
 class DestinationsController < ApplicationController
+  before_action :require_login, only: %i[favorites]
 
   def create
     place_data = JSON.parse(params[:place_data])
 
     @destination = Destination.find_or_create_from_place_data(place_data)
-    if @destination.save
-      redirect_to destination_path(@destination, place_id: @destination.google_maps_place_id)
+    if @destination || !@destination.new_record?
+      redirect_to destination_path(@destination)
     else
-      flash.now[:alert] = "目的地の取得に失敗しました。"
-      render searchs_result_path
+      flash.now[:alert] = "目的地の取得に失敗しました。ページをリロードしてから再度お試し下さい。"
+      redirect_to :search_results
     end
   end
 
   def show
-    @destination = Destination.find_by(google_maps_place_id: params[:place_id])
+    @destination = Destination.find(params[:id])
   end
 
-  def favorite_destinations
-    @favorite_destinations = Destination.find_by(id: params[:id])
+  def favorites
+    @favorite_destinations = current_user.destinations.includes(:favorite_destinations).order(created_at: :desc)
   end
-
 
   private
 
