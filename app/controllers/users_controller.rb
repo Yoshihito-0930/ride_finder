7b4dcf39e6b0c1a6a972e56a313c654574cddf77
Yoshihito <930yoshihito@gmail.com>
2024-08-23 22:44:48 +0900

    userコントローラーにストロングパラメーターを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
   before_action :set_user
-  before_action :require_login, only: %i[show]
+  before_action :require_login, only: %i[show edit]
 
   def show
   end
+
+  def edit
+  end
+
+  def update
+    @user = User.
+  end
+
+
 
   private
 
   def set_user
     @user = User.find_by(id: session[:user_id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :image)
   end
 end
