0300dd022426d58798c04feda5454bd978b667c0
Yoshihito <930yoshihito@gmail.com>
2024-11-17 07:13:35 +0900

    お気に入り、後で行く追加削除時にフラッシュメッセージ追加

--- app/controllers/favorite_destinations_controller.rb
+++ app/controllers/favorite_destinations_controller.rb
@@ -4,12 +4,14 @@ class FavoriteDestinationsController < ApplicationController
   def create
     destination = Destination.find(params[:destination_id])
     current_user.add_favorite(destination)
+    flash[:success] = 'お気に入りに追加しました'
     redirect_to destination_path(destination)
   end
 
   def destroy
     destination = current_user.favorites.find(params[:destination_id])
     current_user.unfavorite(destination)
+    flash[:info] = 'お気に入りから削除しました'
     redirect_to destination_path(destination)
   end
 end
--- app/controllers/future_visits_controller.rb
+++ app/controllers/future_visits_controller.rb
@@ -4,12 +4,14 @@ class FutureVisitsController < ApplicationController
   def create
     destination = Destination.find(params[:destination_id])
     current_user.add_future_visit(destination)
+    flash[:success] = '後で行きたいリストに追加しました'
     redirect_to destination_path(destination)
   end
 
   def destroy
     destination = current_user.upcoming_destinations.find(params[:destination_id])
     current_user.remove_future_visit(destination)
+    flash[:info] = '後で行きたいリストから削除しました'
     redirect_to destination_path(destination)
   end
 end
