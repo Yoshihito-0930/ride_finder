240f90db2c42729d8939316295a77e260a66fa23
Yoshihito <930yoshihito@gmail.com>
2024-12-22 21:43:26 +0900

    ciの設定変更

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_tree ../builds
 //= link application.tailwind.css
-//= link search.css//= link_tree ../../javascript .js
+//= link search.css
+//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
